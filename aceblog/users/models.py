ade91d392244c6a7e8f29c549d549c65e02794d8
Pacchu <itspacchu@users.noreply.github.com>
2020-05-26 15:29:42 +0530

    fixed some issues

--- aceblog/users/models.py
+++ aceblog/users/models.py
@@ -9,12 +9,13 @@ class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     image = models.ImageField(default='default.png',upload_to='profile_pics')
     bio = models.TextField(default=random.choice(random_bios))
-
     def __str__(self):
         return f'{self.user}'
 
 class CollegeDataBase(models.Model):
-    rollno = models.CharField(max_length=10,default="1XAG1A0XXX")
-    is_cr = models.BooleanField(default=False)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    clg_name = models.CharField(User,default=" ",max_length=100)
+    rollno = models.CharField(User,max_length=10,default="1XAG1A0XXX")
+    is_cr = models.BooleanField(User,default=False)
     def __str__(self):
         return f'{self.rollno} Data'
--- aceblog/users/signals.py
+++ aceblog/users/signals.py
@@ -7,7 +7,7 @@ from .models import Profile,CollegeDataBase
 def create_profile(sender,instance,created,**kwargs):
     if created:
         Profile.objects.create(user=instance)
-        CollegeDataBase.objects.create(user=instance)
+        CollegeDataBase.objects.create()
 
 @receiver(post_save,sender=User)
 def save_profile(sender,instance,**kwargs):
