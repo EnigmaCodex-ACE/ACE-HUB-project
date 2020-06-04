1a75e68923863b2b174251cfa6be94b925976451
Pacchu <itspacchu@users.noreply.github.com>
2020-06-04 11:12:14 +0530

    uwu

--- aceblog/blog/models.py
+++ aceblog/blog/models.py
@@ -4,11 +4,17 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 # Create your models here.
 
+
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
+    post_img = models.ImageField(default='',upload_to='post_images',verbose_name='Insert an Image/GIF',blank=True)
+
 
     def __str__(self):
         return self.title
@@ -16,11 +22,24 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post-detail',kwargs={'pk':self.pk})
 
+
+class BG_Img(models.Model):
+    background_img = models.ImageField(default='',upload_to='background_images',blank=True)
+
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    content = models.TextField(max_length=200)
-    timestamp = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    text = models.TextField(max_length=300,help_text='Enter your comment',verbose_name='')
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=True)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
 
     def __str__(self):
-        return f'{self.post.title}-{self.user.username}'
+        return self.text
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
